45a58d2cf7f46b55b44f379eda0d94d35711362b
Aylon <aylonc1@gmail.com>
2023-08-17 13:00:21 +0300

    push

---
+++ src/pages/dashboard/dashboard.tsx
@@ -0,0 +1,8 @@
+import React,{ReactElement} from 'react'
+import "./dashboard.css";
+
+const Dashboard:React.FC = ():ReactElement=>{
+  return (<div>dashboard</div>)
+}
+
+export default Dashboard;
---
+++ src/pages/landingPage/landingPage.tsx
@@ -0,0 +1,8 @@
+import React,{ReactElement} from 'react'
+import "./landingPage.css";
+
+const LandingPage:React.FC = ():ReactElement=>{
+  return (<div>LandingPage</div>)
+}
+
+export default LandingPage;
